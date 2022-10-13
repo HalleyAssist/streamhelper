b383b5b1293cd41c85d23b27b0a6118592884441
Mathew Heard <splitice@users.noreply.github.com>
2022-10-13 22:06:20 +1100

    add tests and fix bug

---
+++ test/test-readuntil.js
@@ -0,0 +1,28 @@
+const chai = require("chai"),
+    Stream = require("stream"),
+    StreamHelper = require('..')
+
+chai.use(require("chai-as-promised"));
+
+describe("readUntil", function(){
+    it("should read until newline", function(){
+        const stream = new Stream.PassThrough()
+        const ret = StreamHelper.streamReadUntil(stream, chunk=>chunk.toString() == "\n")
+        stream.write('Hello World\n')
+        
+        return chai.expect(ret.then(a=>a.toString("utf8"))).to.eventually.equal("Hello World\n")
+    })
+    it("should read until newline and no further", async function(){
+        const stream = new Stream.PassThrough()
+        let ret = StreamHelper.streamReadUntil(stream, chunk=>chunk.toString() == "\n")
+        stream.write("Hello World\nThis is not for us\n")
+        
+        await chai.expect(ret.then(a=>a.toString("utf8"))).to.eventually.equal("Hello World\n")
+
+        ret = StreamHelper.streamReadUntil(stream, chunk=>{
+            return chunk.toString() == "\n"
+        })
+
+        await chai.expect(ret.then(a=>a.toString("utf8"))).to.eventually.equal("This is not for us\n")
+    })
+})
