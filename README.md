d0678a4f8aa992a58b0eabc28d83780a953c053b
Mathew Heard <splitice@users.noreply.github.com>
2022-10-13 14:25:14 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# streamhelper
+A collection of simple stream helper functions in an easy to use class
---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports = require('./lib/StreamHelper')
+module.exports.SyncFunction = require('./lib/StreamHelper')
