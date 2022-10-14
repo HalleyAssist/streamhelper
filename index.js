6d453527eef67e248fb5bc7e4951f41f9a0be048
Mathew Heard <splitice@users.noreply.github.com>
2022-10-14 11:32:49 +1100

    ...

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = require('./lib/StreamHelper')
-module.exports.SyncFunction = require('./lib/StreamHelper')
+module.exports.StreamHelper = require('./lib/StreamHelper')
 module.exports.StreamReader = require('./lib/StreamReader')
 module.exports.EndOfStreamError = require('./lib/EndOfStreamError')
